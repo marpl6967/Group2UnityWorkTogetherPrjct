860626c88b5f907962f45c84cee77092dd9c0304
marpl6967 <72100598+marpl6967@users.noreply.github.com>
2021-04-01 20:09:19 -0500

    Did First half Of Page 1
    Did Page 1

---
+++ New Unity Project/Assets/Scripts/CamManagerScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CamManagerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnSliderValueChanged(float value)
+    {
+        Debug.LogFormat("Slider:{0}", value);
+        // Code will go here
+    }
+
+    public void SwitchViews(Dropdown change)
+    {
+        Debug.LogFormat("{0}:{1}", change.itemText.text,
+            change.value);
+        // Code will go here
+
+
+    }
+}
